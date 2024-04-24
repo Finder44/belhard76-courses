0acd0e6e510657b99e37bc5af897a4608c89ded9
Funder <kirillracuba2005@gmail.com>
2024-04-24 11:22:54 +0300

    for homework.formatting

--- lesson_3/task3_3.py
+++ lesson_3/task3_3.py
@@ -1,7 +1,10 @@
-name = input()
+name = input().title()
 age = int(input())
-city = input()
+city = input().title()
+is_human = True
 print("Your name", name, "your age", age, "you live in", city)  # е подойдет?
 print(f"Name {name} , age {age} , city {city}")
-print("Hello , {first_name} , you are {your_age} , live in {living_cyti}".format(first_name=name, your_age=age,
+print("Hello {first_name} , you are {your_age} and live in {living_cyti}".format(first_name=name, your_age=age,
                                                                                  living_cyti=city))
+text = "Hello %s age=%s city=%s human?=%s" % (name, age, city, is_human)  # почему при 3х не работает
+print(text)
