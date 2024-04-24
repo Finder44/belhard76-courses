b888a0f0bf785b7d0b087cb577d2dae44c34f908
Funder <kirillracuba2005@gmail.com>
2024-04-24 10:44:22 +0300

    for homework.switch " " on "-"

---
+++ lesson_3/task3_2.py
@@ -0,0 +1,12 @@
+# Пользователь вводит 3 числа, найти среднее арифмитическое с
+# точность 3
+numbs = input("Введите числа ")
+list_numbs = numbs.split()
+print(int(list_numbs[0])+int(list_numbs[1])+int(list_numbs[2]))
+# если не с 3 числами
+numbers = input()
+sums= sum(map(int , numbers.split(' ')))
+print(sums)
+# split(' ')разделит все числа через пробел
+# map(int) переводит числа в int
+# sum() сума
