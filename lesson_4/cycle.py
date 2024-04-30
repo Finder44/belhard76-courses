0d831e4da6a5a3c0983a5854080073b58d191605
Funder <kirillracuba2005@gmail.com>
2024-04-30 19:04:35 +0300

    for homework.

--- lesson_4/cycle.py
+++ lesson_4/cycle.py
@@ -1,6 +1,4 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n).
-import math
 numbers = int(input("Введите сьепенть числа 2 "))
-g=0
-degree = [pow(2, numbers) for i in range(1,numbers+2)]
+degree = [2 ** i for i in range(1,numbers+1)]
 print(degree)
