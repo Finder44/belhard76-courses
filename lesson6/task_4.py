d0e22f07626cf82ddaab16768e832fe9fefe2cb3
Funder <kirillracuba2005@gmail.com>
2024-05-12 16:25:43 +0300

    for homework.

--- lesson6/task_4.py
+++ lesson6/task_4.py
@@ -7,3 +7,8 @@ mixed_list = [1, "apple", 3.14, True, "banana", None, "cherry", False, "date"]
 filtered_list = [x for x in mixed_list if isinstance(x, str)]
 
 print(filtered_list)
+
+for index in range(len(text)):
+    if not isinstance(text[index],str):
+        text.pop(index)
+print(text)
