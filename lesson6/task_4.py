439774b4a8ddc6e398fdd427a59fa648494bf3ec
Funder <kirillracuba2005@gmail.com>
2024-05-12 16:20:45 +0300

    for homework.

--- lesson6/task_4.py
+++ lesson6/task_4.py
@@ -1,3 +1,9 @@
 text = ["man" ,2 ,2 ,4, 5, " alfa " , "bet"]
 text = list(filter(lambda x: isinstance(x,str), text))
 print(text)
+
+mixed_list = [1, "apple", 3.14, True, "banana", None, "cherry", False, "date"]
+
+filtered_list = [x for x in mixed_list if isinstance(x, str)]
+
+print(filtered_list)
